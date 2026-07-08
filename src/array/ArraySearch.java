0e5e7bf60e3510df8a57dd12b85f70c6a8f2e7b2
Rishikesh Jha <rishikesh.in@gmail.com>
2026-07-09 03:46:40 +0530

    Add linear search implementation

--- src/array/ArraySearch.java
+++ src/array/ArraySearch.java
@@ -1,4 +1,29 @@
+/*
+Problem: Array Search (Linear Search)
+
+Checks whether a target element exists in an array using a linear scan.
+
+Time Complexity : O(n)
+Space Complexity: O(1)
+*/
+
 package array;
 
-public class ArraySearch {
+public class  ArraySearch {
+    public static boolean arraySearch(int[] arr,int target){
+        for(int x : arr){
+            if(x == target){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(arraySearch(new int[]{1,4,5,3,6,9},9));
+        System.out.println(arraySearch(new int[]{10}, 10));
+        System.out.println(arraySearch(new int[]{2,4,6,8}, 5));
+        System.out.println(arraySearch(new int[]{-5,-2,-10,-1}, -10));
+        System.out.println(arraySearch(new int[]{1,2,3,2,5,2}, 2));
+        System.out.println(arraySearch(new int[]{}, 5));
+    }
 }
